91909f15ee02b64b8d3975071b4230e8378d3b4d
cbsardina <cbsardina@gmail.com>
2017-11-01 01:59:53 -0400

    DDP: email2 - push before first email test.

--- back-end/AR-FinalProject/src/main/java/com/arproject/arproject/controller/MailController.java
+++ back-end/AR-FinalProject/src/main/java/com/arproject/arproject/controller/MailController.java
@@ -1,11 +1,17 @@
 package com.arproject.arproject.controller;
 
+import com.arproject.arproject.model.MailObject;
 import com.arproject.arproject.service.EmailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +40,16 @@ public class MailController {
     @GetMapping("/mail/home")
     public String mailHome(){
         return "mailHome";
+    }
+
+    @PostMapping("/mail/sendSimple")
+    public String sendSimpleEmail(Model model,
+                                  @ModelAttribute("mailObject") @Valid MailObject mailObject, Errors err) {
+        if (err.hasErrors()) {
+            return "emailError";
+        }
+        emailService.sendSimpleMessage(mailObject.getTo(), mailObject.getSubject(), mailObject.getText());
+        return "emailSuccess";
     }
 
 }
